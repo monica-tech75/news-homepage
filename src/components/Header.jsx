b1d1d5db3e8682fab685eef64f925c7e0a456d3d
Monica Pardo <moonyruna3@gmail.com>
2025-09-26 18:26:02 +0200

    Corregir error en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,14 @@
-
-import logo from '../assets/images/logo.svg'
-import { NavBar } from './NavBar'
-
+import logo from '../assets/images/logo.svg';
+import { NavBar } from './NavBar';
 
 export const Header = () => {
   return (
-
-    <header className="bg-white flex justify-between py-4">
-      <img src={logo} alt="logo with the letter M" />
+    <header className='bg-white flex justify-between py-4'>
+      <img
+        src={logo}
+        alt='logo with the letter W'
+      />
       <NavBar />
     </header>
-    
-  )
-}
-
+  );
+};
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,44 @@
-import menuLogo from '../assets/images/icon-menu.svg'
+import { useState } from 'react';
+
+import menuLogo from '../assets/images/icon-menu.svg';
 
 export const NavBar = () => {
+  const [isHeaderOpen, setIsHeaderOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsHeaderOpen(!isHeaderOpen);
+  };
   return (
     <>
-  <ul className="hidden md:flex justify-end gap-10 text-[0.938rem] md:w-[55vw] md:items-center">
-      <li className='hover:text-myred-400'><a href="#">Home</a></li>
-      <li className='hover:text-myred-400'><a href="#">New</a></li>
-      <li className='hover:text-myred-400'><a href="#">Popular</a></li>
-      <li className='hover:text-myred-400'><a href="#">Trending</a></li>
-      <li className='hover:text-myred-400'><a href="#">Categories</a></li>
-    </ul>
-    <img className="md:hidden" src={menuLogo} alt="logo for menu burger"/>
+      <ul
+        className={`md:flex justify-end gap-10 text-[0.938rem] md:w-[55vw] md:items-center ${
+          isHeaderOpen ? 'block' : 'hidden'
+        } md:block`}>
+        <li className='hover:text-myred-400'>
+          <a href='#'>Home</a>
+        </li>
+        <li className='hover:text-myred-400'>
+          <a href='#'>New</a>
+        </li>
+        <li className='hover:text-myred-400'>
+          <a href='#'>Popular</a>
+        </li>
+        <li className='hover:text-myred-400'>
+          <a href='#'>Trending</a>
+        </li>
+        <li className='hover:text-myred-400'>
+          <a href='#'>Categories</a>
+        </li>
+      </ul>
+      <button
+        onClick={toggleMenu}
+        className='md:hidden'>
+        {' '}
+        <img
+          src={menuLogo}
+          alt='logo for menu burger'
+        />{' '}
+      </button>
     </>
-    
-  )
-}
-
+  );
+};
